c1438123aa928c5f76ef2d9a80845850d6248f52
Shum-ap <apet5685@gmail.com>
2025-07-23 13:09:04 +0200

    переименование

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-# config.py
 import os
 from dotenv import load_dotenv
 
@@ -11,8 +10,15 @@ MONGO_HOST = os.getenv("MONGO_HOST")
 MONGO_DB = os.getenv("MONGO_DB")
 MONGO_COLLECTION = os.getenv("MONGO_COLLECTION")
 
-# Сформированный URI
 MONGO_URI = (
     f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}/"
     f"?readPreference=primary&ssl=false&authMechanism=DEFAULT&authSource={MONGO_DB}"
 )
+
+# MySQL
+MYSQL_CONFIG = {
+    "host": os.getenv("MYSQL_HOST"),
+    "user": os.getenv("MYSQL_USER"),
+    "password": os.getenv("MYSQL_PASSWORD"),
+    "database": os.getenv("MYSQL_DATABASE"),
+}
